c57fc8d6e8faea1c6e05c5fef2bc3d93f9d7761c
NASSIM BOUSSAID <NASSIM.BOUSSAID.CSM@gmail.com>
2024-06-10 15:29:04 +0200

    useless selectOneById in QuestionManager

--- src/Model/QuestionManager.php
+++ src/Model/QuestionManager.php
@@ -38,13 +38,4 @@ class QuestionManager extends AbstractManager
         }
         return $questionId;
     }
-
-    public function selectOneById(int $id): array|false
-    {
-        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
-    }
 }
